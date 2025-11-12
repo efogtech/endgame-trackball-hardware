cf886ae8a9f0fd751e6afa8cf03eb3230faa467f
Artem Suprun <efogdev@gmail.com>
2025-11-12 06:42:31 +0200

    feat: encoders upgrade; PAW3395 bb; PAW3805 bb

---
+++ encoders-upgrade/panelize.sh
@@ -0,0 +1,7 @@
+kikit panelize \
+    --layout 'vspace: 2mm; rows: 7; cols: 3' \
+    --cuts 'type: vcuts' \
+    --copperfill 'type: solid' \
+    --post 'millradiusouter: 0.5mm' \
+    '/home/efog/Projects/endgame-trackball/endgame-trackball-hardware/encoders-upgrade/encoders_upgrade.kicad_pcb' '/home/efog/Projects/endgame-trackball/endgame-trackball-hardware/encoders-upgrade/panel'
+
